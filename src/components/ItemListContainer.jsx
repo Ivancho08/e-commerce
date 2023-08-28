ac1e8feb746bfa8503c55b455b2dfd87e675b39c
Ivancho08 <ivansantamaria812@gmail.com>
2023-08-28 17:09:07 -0300

    Creando Json y utilizando la data

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,41 @@
 import { Container } from "react-bootstrap"
+import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import data from "../data/products.json";
+
 
 export const ItemListContainer = (props) => {
+
+    const [products, setPrducts] = useState([]);
+
+    useEffect(() => {
+        const promise = new Promise((resolve,reject) => {
+            setTimeout(() => resolve(data), 2000);
+        })
+
+        promise.then(data => setPrducts(data))
+    }, [])
+
 
     return (
         <Container className="mt-4">
             {props.greeting}
+            <div>
+                {products.map(products => (
+                    <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                ))}
+            </div>
         </Container>
     )
 }
